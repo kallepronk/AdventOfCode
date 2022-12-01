27818af10c17dd14e735cc117f1a8755750846b8
Kalle Pronk <kallepronk@gmail.com>
2022-12-01 18:01:58 +0100

    day 1 level 2

--- 2022/day-1/src/main.rs
+++ 2022/day-1/src/main.rs
@@ -8,18 +8,22 @@ fn main() {
     if let Ok(lines) = read_lines(path) {
         // Consumes the iterator, returns an (Optional) String
         let mut my_int:i32 = 0;
-        let mut biggest:i32 = 0;
+        let mut vec:Vec<i32> = Vec::new();
         for line in lines {
             if let Ok(ip) = line {
                 match ip.parse::<i32>() {
                     Ok(n) => {
                         my_int += n;
                     },
-                    Err(_e) => {if my_int > biggest {biggest = my_int} my_int = 0;},
+                    Err(_e) => { vec.push(my_int); my_int = 0;},
                 }
             }
         }
-        println!("Biggest calorie count: {}", biggest);
+
+        vec.sort();
+        let vec2 = vec.split_off(vec.len() - 3);
+        let sum:i32 = vec2.iter().sum();
+        println!("Biggest calorie count: {}", sum);
     }
     
 }
